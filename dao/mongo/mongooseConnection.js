b6fb2e27efa6efe368c4d8b55f723961dd413920
Paulo-Estalise <phestalise@gmail.com>
2024-10-08 20:55:24 -0300

    correçãoMOngo

--- dao/mongo/mongooseConnection.js
+++ dao/mongo/mongooseConnection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const mongooseConnection = async () => {
     try {
         // Substitua 'sua_string_de_conexão_aqui' pela sua string de conexão real
-        await mongoose.connect('sua_string_de_conexão_aqui');
+        await mongoose.connect('mongodb://localhost:27017/ecommerce');
+
         console.log('MongoDB conectado com sucesso!');
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB:', error);
